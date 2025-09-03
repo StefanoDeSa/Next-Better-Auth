d288988cc2a2636aea25e16eae702f19c20005f3
StefanoDeSa <stefanobritodesa@gmail.com>
2025-09-03 17:51:02 -0300

    Adiciona projeto

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        resetPassword: {
+            url: "http://localhost:3000/reset-password",
+        }
+    },
+    socialProviders: {
+        google: {
+            enabled: true,
+            clientId: ""
+        }
+    },
+
+});
